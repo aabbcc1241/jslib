81538dd8793c453a6e371efa3267c999491a7d12
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-07-15 19:07:42 +0800

    added dist into repo

---
+++ dist/horizon-param.d.ts
@@ -0,0 +1,6 @@
+declare module horizon {
+    const ASCENDING: string;
+    const DESCENDING: string;
+    const INCLUSIVE: string;
+    const EXCLUSIVE: string;
+}
---
+++ dist/horizon-param.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by beenotung on 5/28/16.
+ */
+
+module horizon {
+  export const ASCENDING:string = "ascending";
+  export const DESCENDING:string = "descending";
+  export const INCLUSIVE:string = "closed";
+  export const EXCLUSIVE:string = "open";
+}
