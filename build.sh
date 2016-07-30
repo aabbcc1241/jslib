6ca328637defa598339257d906ea1841e283bcf4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-07-30 21:51:49 +0800

    added style into flow; updated package.json keyword

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-#npm install && npm run build # deprecated
-
-cd es5 && npm install && npm run build && cd ..
-cd es6 && npm install && npm run build && cd ..
+npm install && npm run build
