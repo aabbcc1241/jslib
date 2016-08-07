f6537e27db87da6b3a553419508062b4490a42ed
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-08-08 00:00:38 +0800

    updated es6 to support nodejs module

--- es6/src/utils-es6.ts
+++ es6/src/utils-es6.ts
@@ -1,86 +1,112 @@
 /**
  * Created by beenotung on 7/29/16.
  */
-///<reference path="../../es5/src/utils-es5.ts"/>
+import {PROTOTYPE, NOOP, noop} from "../../es5/dist/utils-es5";
 
-module require {
-  type PendingCallback=[NOOP,NOOP];
-  const pending = new Map<string,PendingCallback[]>();
-  const cached = new Set<string>();
+var fetch:IFetchStatic;
+fetch = fetch || eval("require('isomorphic-fetch')");
 
-  /**@return file extension*/
-  async function checkSource(url:string, cors:boolean):Promise<string> {
-    if (url.indexOf('.') == -1)
-      throw new URIError('no sub-filename detected');
-    let option = <RequestInit>{
-      mode: cors ? "cors" : "no-cors"
-      , cache: "force-cache"
-    };
-    let response = await fetch(url, option);
-    let text = await response.text();
-    if (text.length == 0) {
-      throw new TypeError('empty file')
-    } else {
-      return url.split('.').pop();
+declare var require:(url:string)=>any;
+let _require = require;
+
+module jslib {
+
+  /* mainly to run in browser */
+  export module require {
+    type PendingCallback=[NOOP,NOOP];
+    const pending = new Map<string,PendingCallback[]>();
+    const cached = new Set<string>();
+
+    /**@return file extension*/
+    async function checkSource(url:string, cors:boolean):Promise<[string,string]> {
+      if (url.indexOf('.') == -1)
+        throw new URIError('no sub-filename detected');
+      let option = <RequestInit>{
+        mode: cors ? "cors" : "no-cors"
+        , cache: "force-cache"
+      };
+      let response = await fetch(url, option);
+      let text:string = await response.text();
+      if (text.length == 0) {
+        throw new TypeError('empty file')
+      } else {
+        return <[string,string]>[url.split('.').pop(), text];
+      }
+    }
+
+    async function injectSource(url:string, cors:boolean, scopedEval?:(code:string)=>void):Promise<string> {
+      let [filetype,filecontent] = await checkSource(url, cors);
+      return new Promise((resolve:NOOP, reject:NOOP)=> {
+        if (typeof window === 'undefined')
+          if (filetype == 'js') {
+            let result:any;
+            if (scopedEval)
+              scopedEval(filecontent);
+            else
+              eval(filecontent);
+            resolve(result);
+          }
+          else
+            reject(new TypeError('invalid file type (server side nodejs) :' + filetype));
+        else
+          switch (filetype) {
+            case 'js':
+              let script = document.createElement('script');
+              script.onload = ()=>resolve(filecontent);
+              script.onerror = reject;
+              script.async = true;
+              script.src = url;
+              document.head.appendChild(script);
+              break;
+            case 'css':
+              let link = document.createElement('link');
+              link.rel = 'stylesheet';
+              link.href = url;
+              document.head.appendChild(link);
+              break;
+            default:
+              reject(new TypeError('invalid file type :' + filetype));
+          }
+      });
+    }
+
+    export function load(url:string, cors = false, scopedEval:(code:string)=>void) {
+      /* check if in nodejs or browser */
+      return new Promise((resolve:NOOP, reject:NOOP)=> {
+        if (cached.has(url)) {
+          resolve()
+        } else {
+          if (pending.has(url)) {
+            pending.get(url).push([resolve, reject])
+          } else {
+            let xss = [<PendingCallback>[resolve, reject]];
+            pending.set(url, xss);
+            injectSource(url, cors, scopedEval)
+              .then((code?:string)=> {
+                cached.add(url);
+                xss.forEach(xs=>xs[0](code));
+                pending.delete(url);
+              })
+              .catch((e)=> {
+                xss.forEach(xs=>xs[1](e));
+                pending.delete(url);
+              });
+          }
+        }
+      });
     }
   }
 
-  async function injectSource(url:string, cors:boolean) {
-    let filetype = await checkSource(url, cors);
-    return new Promise((resolve, reject)=> {
-      switch (filetype) {
-        case 'js':
-          let script = document.createElement('script');
-          script.onload = resolve;
-          script.onerror = reject;
-          script.async = true;
-          script.src = url;
-          document.head.appendChild(script);
-          break;
-        case 'css':
-          let link = document.createElement('link');
-          link.rel = 'stylesheet';
-          link.href = url;
-          document.head.appendChild(link);
-          break;
-        default:
-          throw new TypeError('invalid file type :' + filetype);
-      }
-    });
+  /**@deprecated use Object.assign and Object.create instaed */
+  export function objectClone<A>(o:any):A {
+    if (o) {
+      var res:any = new noop();
+      Object.assign(res, o);
+      res[PROTOTYPE] = o[PROTOTYPE];
+      return res;
+    } else return o;
   }
 
-  export function load(url:string, cors = false) {
-    return new Promise((resolve:NOOP, reject:NOOP)=> {
-      if (cached.has(url)) {
-        resolve()
-      } else {
-        if (pending.has(url)) {
-          pending.get(url).push([resolve, reject])
-        } else {
-          let xss = [<PendingCallback>[resolve, reject]];
-          pending.set(url, xss);
-          injectSource(url, cors)
-            .then(()=> {
-              cached.add(url);
-              xss.forEach(xs=>xs[0]());
-              pending.delete(url);
-            })
-            .catch(()=> {
-              xss.forEach(xs=>xs[1]());
-              pending.delete(url);
-            });
-        }
-      }
-    });
-  }
 }
 
-/**@deprecated use Object.assign and Object.create instaed */
-function objectClone<A>(o:any):A {
-  if (o) {
-    var res:any = new noop();
-    Object.assign(res, o);
-    res[PROTOTYPE] = o[PROTOTYPE];
-    return res;
-  } else return o;
-}
+export = jslib;
---
+++ es6/test/index.js
@@ -0,0 +1,85 @@
+/**
+ * Created by beenotung on 8/7/16.
+ */
+
+const testname = 'jslib-es6';
+
+console.log('test ' + testname + ' start');
+console.log();
+
+function show(x) {
+  var msg = {};
+  if (eval('typeof ' + x) === 'undefined')
+    msg[x] = undefined;
+  else
+    msg[x] = eval(x);
+  console.log(msg);
+}
+var testModuleList = [];
+function testModule(name, func) {
+  testModuleList.push(new Promise(function (resolve, reject) {
+    console.log();
+    console.log('start', ':', name);
+    console.assert(typeof func === 'function', 'testModule invalid argument, expect testModule(String, Function)');
+    try {
+      var module = require(name);
+      var result = func(module);
+      Promise.resolve(result).then(function () {
+        console.log('success', ':', name);
+        resolve();
+      }).catch(function () {
+        console.error('fail', ':', name);
+        console.error(e);
+        reject();
+      })
+    } catch (e) {
+      console.error('fail', ':', name);
+      console.error(e);
+      rejectj();
+    }
+    console.log();
+  }));
+}
+
+
+show('Horizon');
+testModule('../dist/utils-es6', function (u6) {
+  return new Promise(function (resolve, reject) {
+    var a = {a: 1};
+    var b = u6.objectClone(a);
+    console.assert(a.toString() == b.toString(), 'objectClone not working');
+    console.assert(typeof u6.require === "object", 'jslib.require does not exist?');
+    // var url = 'https://clients5.google.com/pagead/drt/dn/dn.js';
+    var url = 'http://127.0.0.1:8181/horizon/horizon.js';
+    // var url = 'http://yourjavascript.com/8181617142/test.js'; //pure js
+    // var url = 'http://yourjavascript.com/1287911263/test.js'; //module js
+    u6.require.load(url, void 0, eval)
+      .then(function () {
+        console.log('loaded js file');
+        try {
+          // console.assert(typeof test === 'function', 'test function inside the required js file is not loading successful?');
+          // console.log('test function', test.toString());
+          show('Horizon');
+          resolve();
+        } catch (e) {
+          console.error('falied to handle load result', e);
+          reject(e)
+        }
+      })
+      .catch(function (error) {
+        console.error('load failed', ':', error);
+        reject();
+      });
+  });
+});
+
+Promise.resolve.apply(Promise, testModuleList)
+  .then(function () {
+    console.log();
+    console.log('test ' + testname + ' end, all success');
+  })
+  .catch(function () {
+    console.log();
+    console.log('test ' + testname + ' end, some failed');
+  });
+
